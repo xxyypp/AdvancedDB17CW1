3ab6d624753a9dd30e1f35c039f12894d4814111
xxyypp <yangpuypyp@outlook.com>
2017-10-26 22:21:08 +0100

    update

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 echo "-------------------------------------"
 git add .
 git commit -m "update"
-git push origin master
+git push origin master --repo https://xxyypp:yp200813@github.com/xxyypp/AdvancedDB17CW1.git
 echo "Updated..."
 echo "-------------------------------------"
