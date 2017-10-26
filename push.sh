7eecf774f1d3ba660347401960a9c46839b46b1b
xxyypp <yangpuypyp@outlook.com>
2017-10-26 22:09:43 +0100

    update

---
+++ push.sh
@@ -0,0 +1,8 @@
+echo "-------------------------------------"
+git add .
+git commit -m "update"
+git push origin master
+cat "xxyypp"
+cat "yp200813"
+echo "Updated..."
+echo "-------------------------------------"
